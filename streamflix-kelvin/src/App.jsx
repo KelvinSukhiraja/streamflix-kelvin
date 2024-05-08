ae621496fd4e50957bb3dd59ce968dea0edbf4c7
Kelvin <hologram171@gmail.com>
2024-05-09 00:12:16 +0700

    deployment

--- streamflix-kelvin/src/App.jsx
+++ streamflix-kelvin/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Home from "./pages/Home";
 import Movie from "./pages/Movie";
 import { Route, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <>
+      <Toaster closeButton richColors />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:Id/:Title" element={<Movie />} />
